d7fa491dfcef287ae2e40908b5f981300bd1a78b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-22 11:46:34 +0300

    initial commit

---
+++ VB/WinWebSolution.Module/Employee.vb
@@ -0,0 +1,211 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports DevExpress.Xpo
+Imports System.Drawing
+Imports System.Security
+Imports System.ComponentModel
+Imports System.Collections.Generic
+Imports DevExpress.Persistent.Base
+Imports DevExpress.ExpressApp.Filtering
+Imports DevExpress.Persistent.Validation
+Imports DevExpress.Persistent.Base.General
+Imports DevExpress.Persistent.Base.Security
+
+Namespace WinWebSolution.Module
+    <NavigationItem(), ImageName("BO_User"), DefaultProperty("UserName")> _
+    Public Class Employee
+        Inherits BasePersistentObject
+        Implements IPerson, IUser, IUserWithRoles, IAuthenticationActiveDirectoryUser, IAuthenticationStandardUser, IResource
+        Private _FirstName As String
+        Private _Email As String
+        Private _LastName As String
+        Private _MiddleName As String
+        Private _Birthday As DateTime
+        Private _IsActive As Boolean = True
+        Private _ChangePasswordOnFirstLogon As Boolean
+        Private _Permissions As List(Of IPermission)
+        <Persistent("Color")> _
+        Private _Color As Integer
+        Private _Caption As String
+        Private _UserName As String
+        Private _StoredPassword As String
+        Public Sub New(ByVal session As Session)
+            MyBase.New(session)
+            _Permissions = New List(Of IPermission)()
+        End Sub
+        Public Overrides Sub AfterConstruction()
+            MyBase.AfterConstruction()
+            _Color = Color.White.ToArgb()
+        End Sub
+        <NonPersistent()> _
+        Public Property Color() As Color
+            Get
+                Return Color.FromArgb(_Color)
+            End Get
+            Set(ByVal value As Color)
+                SetPropertyValue("Color", _Color, value.ToArgb())
+            End Set
+        End Property
+        <Association("Employee-Group", UseAssociationNameAsIntermediateTableName:=True), RuleRequiredField(Nothing, DefaultContexts.Save, TargetCriteria:="IsActive", CustomMessageTemplate:="You cannot save an Active employee without Groups")> _
+        Public ReadOnly Property Groups() As XPCollection(Of Group)
+            Get
+                Return GetCollection(Of Group)("Groups")
+            End Get
+        End Property
+        <Association("Activity-Employees", UseAssociationNameAsIntermediateTableName:=True)> _
+        Public ReadOnly Property Activities() As XPCollection(Of Activity)
+            Get
+                Return GetCollection(Of Activity)("Activities")
+            End Get
+        End Property
+        <MemberDesignTimeVisibility(False)> _
+        Public Property StoredPassword() As String
+            Get
+                Return _StoredPassword
+            End Get
+            Set(ByVal value As String)
+                SetPropertyValue("StoredPassword", _StoredPassword, value)
+            End Set
+        End Property
+#Region "IUserWithRoles Members"
+        Private ReadOnly Property IUserWithRoles_Roles() As IList(Of IRole) Implements IUserWithRoles.Roles
+            Get
+                Return New ListConverter(Of IRole, Group)(Groups)
+            End Get
+        End Property
+#End Region
+#Region "IUser Members"
+        Public Property IsActive() As Boolean Implements IUser.IsActive
+            Get
+                Return _IsActive
+            End Get
+            Set(ByVal value As Boolean)
+                SetPropertyValue("IsActive", _IsActive, value)
+            End Set
+        End Property
+        Public ReadOnly Property Permissions() As IList(Of IPermission) Implements IUser.Permissions
+            Get
+                _Permissions.Clear()
+                For Each role As IRole In Groups
+                    _Permissions.AddRange(role.Permissions)
+                Next role
+                Return _Permissions.AsReadOnly()
+            End Get
+        End Property
+        Public Sub ReloadPermissions() Implements IUser.ReloadPermissions
+            Groups.Reload()
+            For Each group As Group In Groups
+                group.PersistentPermissions.Reload()
+            Next group
+        End Sub
+        Private ReadOnly Property IUser_UserName() As String Implements IUser.UserName
+            Get
+                Return UserName
+            End Get
+        End Property
+#End Region
+#Region "IAuthenticationActiveDirectoryUser Members"
+        <RuleRequiredField(Nothing, "Save", "The user name must not be empty"), RuleUniqueValue(Nothing, "Save", "The login with the entered UserName was already registered within the system")> _
+        Public Property UserName() As String Implements IAuthenticationActiveDirectoryUser.UserName
+            Get
+                Return _UserName
+            End Get
+            Set(ByVal value As String)
+                SetPropertyValue("UserName", _UserName, value)
+            End Set
+        End Property
+#End Region
+#Region "IAuthenticationStandardUser Members"
+        Private ReadOnly Property IAuthenticationStandardUser_UserName() As String Implements IAuthenticationStandardUser.UserName
+            Get
+                Return UserName
+            End Get
+        End Property
+        Public Property ChangePasswordOnFirstLogon() As Boolean Implements IAuthenticationStandardUser.ChangePasswordOnFirstLogon
+            Get
+                Return _ChangePasswordOnFirstLogon
+            End Get
+            Set(ByVal value As Boolean)
+                SetPropertyValue("ChangePasswordOnFirstLogon", _ChangePasswordOnFirstLogon, value)
+            End Set
+        End Property
+        Public Function ComparePassword(ByVal password As String) As Boolean Implements IAuthenticationStandardUser.ComparePassword
+            Return New PasswordCryptographer().AreEqual(StoredPassword, password)
+        End Function
+        Public Sub SetPassword(ByVal password As String) Implements IAuthenticationStandardUser.SetPassword
+            StoredPassword = New PasswordCryptographer().GenerateSaltedPassword(password)
+        End Sub
+#End Region
+#Region "IResource Members"
+        Public Property Caption() As String Implements IResource.Caption
+            Get
+                Return _Caption
+            End Get
+            Set(ByVal value As String)
+                SetPropertyValue("Caption", _Caption, value)
+            End Set
+        End Property
+        <Browsable(False)> _
+        Public ReadOnly Property Id() As Object Implements IResource.Id
+            Get
+                Return Oid
+            End Get
+        End Property
+        <Browsable(False)> _
+        Public ReadOnly Property OleColor() As Integer Implements IResource.OleColor
+            Get
+                Return ColorTranslator.ToOle(Color.FromArgb(_Color))
+            End Get
+        End Property
+#End Region
+#Region "IPerson Members"
+        Public Property FirstName() As String Implements IPerson.FirstName
+            Get
+                Return _FirstName
+            End Get
+            Set(ByVal value As String)
+                SetPropertyValue("FirstName", _FirstName, value)
+            End Set
+        End Property
+        Public Property LastName() As String Implements IPerson.LastName
+            Get
+                Return _LastName
+            End Get
+            Set(ByVal value As String)
+                SetPropertyValue("LastName", _LastName, value)
+            End Set
+        End Property
+        Public Property MiddleName() As String Implements IPerson.MiddleName
+            Get
+                Return _MiddleName
+            End Get
+            Set(ByVal value As String)
+                SetPropertyValue("MiddleName", _MiddleName, value)
+            End Set
+        End Property
+        Public Property Birthday() As DateTime Implements IPerson.Birthday
+            Get
+                Return _Birthday
+            End Get
+            Set(ByVal value As DateTime)
+                SetPropertyValue("Birthday", _Birthday, value)
+            End Set
+        End Property
+        <SizeAttribute(255)> _
+        Public Property Email() As String Implements IPerson.Email
+            Get
+                Return _Email
+            End Get
+            Set(ByVal value As String)
+                SetPropertyValue("Email", _Email, value)
+            End Set
+        End Property
+        <PersistentAlias("concat(FirstName, MiddleName, LastName)"), SearchMemberOptions(SearchMemberMode.Include)> _
+        Public ReadOnly Property FullName() As String Implements IPerson.FullName
+            Get
+                Return Convert.ToString(EvaluateAlias("FullName"))
+            End Get
+        End Property
+#End Region
+    End Class
+End Namespace
---
+++ VB/WinWebSolution.Win/WinApplication.Designer.vb
@@ -0,0 +1,125 @@
+Imports Microsoft.VisualBasic
+Imports System
+Namespace WinWebSolution.Win
+	Partial Public Class WinWebSolutionWindowsFormsApplication
+		''' <summary> 
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary> 
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Component Designer generated code"
+
+		''' <summary> 
+		''' Required method for Designer support - do not modify 
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.module1 = New DevExpress.ExpressApp.SystemModule.SystemModule()
+			Me.module2 = New DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule()
+			Me.module3 = New WinWebSolution.Module.WinWebSolutionModule()
+			Me.module4 = New WinWebSolution.Module.Win.WinWebSolutionWindowsFormsModule()
+			Me.module5 = New DevExpress.ExpressApp.Validation.ValidationModule()
+			Me.module6 = New DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule()
+			Me.module7 = New DevExpress.ExpressApp.Validation.Win.ValidationWindowsFormsModule()
+			Me.securityModule1 = New DevExpress.ExpressApp.Security.SecurityModule()
+			Me.schedulerWindowsFormsModule1 = New DevExpress.ExpressApp.Scheduler.Win.SchedulerWindowsFormsModule()
+			Me.schedulerModuleBase1 = New DevExpress.ExpressApp.Scheduler.SchedulerModuleBase()
+			Me.securityComplex1 = New DevExpress.ExpressApp.Security.SecurityComplex()
+			Me.securityComplex2 = New DevExpress.ExpressApp.Security.SecurityComplex()
+			Me.securityComplex3 = New DevExpress.ExpressApp.Security.SecurityComplex()
+			Me.authenticationStandard1 = New DevExpress.ExpressApp.Security.AuthenticationStandard()
+			Me.authenticationStandard2 = New DevExpress.ExpressApp.Security.AuthenticationStandard()
+			Me.authenticationStandard3 = New DevExpress.ExpressApp.Security.AuthenticationStandard()
+			CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+			' 
+			' module1
+			' 
+			Me.module1.AdditionalExportedTypes.Add(GetType(DevExpress.Xpo.XPObjectType))
+			' 
+			' module5
+			' 
+			Me.module5.AllowValidationDetailsAccess = True
+			' 
+			' securityComplex1
+			' 
+			Me.securityComplex1.Authentication = Nothing
+			Me.securityComplex1.IsGrantedForNonExistentPermission = False
+			Me.securityComplex1.RoleType = Nothing
+			Me.securityComplex1.UserType = Nothing
+			' 
+			' securityComplex2
+			' 
+			Me.securityComplex2.Authentication = Nothing
+			Me.securityComplex2.IsGrantedForNonExistentPermission = False
+			Me.securityComplex2.RoleType = Nothing
+			Me.securityComplex2.UserType = Nothing
+			' 
+			' securityComplex3
+			' 
+			Me.securityComplex3.Authentication = Me.authenticationStandard3
+			Me.securityComplex3.IsGrantedForNonExistentPermission = False
+			Me.securityComplex3.RoleType = GetType(WinWebSolution.Module.Group)
+			Me.securityComplex3.UserType = GetType(WinWebSolution.Module.Employee)
+			' 
+			' authenticationStandard1
+			' 
+			Me.authenticationStandard1.LogonParametersType = GetType(DevExpress.ExpressApp.Security.AuthenticationStandardLogonParameters)
+			' 
+			' authenticationStandard2
+			' 
+			Me.authenticationStandard2.LogonParametersType = GetType(DevExpress.ExpressApp.Security.AuthenticationStandardLogonParameters)
+			' 
+			' authenticationStandard3
+			' 
+			Me.authenticationStandard3.LogonParametersType = GetType(DevExpress.ExpressApp.Security.AuthenticationStandardLogonParameters)
+			' 
+			' WinWebSolutionWindowsFormsApplication
+			' 
+			Me.ApplicationName = "WinWebSolution"
+			Me.Modules.Add(Me.module1)
+			Me.Modules.Add(Me.module2)
+			Me.Modules.Add(Me.module6)
+			Me.Modules.Add(Me.module3)
+			Me.Modules.Add(Me.module4)
+			Me.Modules.Add(Me.module5)
+			Me.Modules.Add(Me.module7)
+			Me.Modules.Add(Me.securityModule1)
+			Me.Modules.Add(Me.schedulerModuleBase1)
+			Me.Modules.Add(Me.schedulerWindowsFormsModule1)
+			Me.Security = Me.securityComplex3
+'			Me.DatabaseVersionMismatch += New System.EventHandler(Of DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs)(Me.WinWebSolutionWindowsFormsApplication_DatabaseVersionMismatch);
+			CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
+		End Sub
+
+		#End Region
+
+		Private module1 As DevExpress.ExpressApp.SystemModule.SystemModule
+		Private module2 As DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule
+		Private module3 As WinWebSolution.Module.WinWebSolutionModule
+		Private module4 As WinWebSolution.Module.Win.WinWebSolutionWindowsFormsModule
+		Private module5 As DevExpress.ExpressApp.Validation.ValidationModule
+		Private module6 As DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule
+		Private module7 As DevExpress.ExpressApp.Validation.Win.ValidationWindowsFormsModule
+		Private securityModule1 As DevExpress.ExpressApp.Security.SecurityModule
+		Private schedulerWindowsFormsModule1 As DevExpress.ExpressApp.Scheduler.Win.SchedulerWindowsFormsModule
+		Private schedulerModuleBase1 As DevExpress.ExpressApp.Scheduler.SchedulerModuleBase
+		Private securityComplex1 As DevExpress.ExpressApp.Security.SecurityComplex
+		Private securityComplex2 As DevExpress.ExpressApp.Security.SecurityComplex
+		Private securityComplex3 As DevExpress.ExpressApp.Security.SecurityComplex
+		Private authenticationStandard1 As DevExpress.ExpressApp.Security.AuthenticationStandard
+		Private authenticationStandard2 As DevExpress.ExpressApp.Security.AuthenticationStandard
+		Private authenticationStandard3 As DevExpress.ExpressApp.Security.AuthenticationStandard
+	End Class
+End Namespace
